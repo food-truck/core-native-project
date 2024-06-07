86e767749618104eb2118a2bbd24b059ef3623ed
Bot Wu <botwu@chancetop.com>
2024-06-07 05:05:17 -0400

    TIW-49 Phase2: Core-native Refactor

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,15 +1,28 @@
 import "react-native-get-random-values";
 import type {PersistInstance} from "./util/LogPersistUtil";
-import {LoggerImpl as CoreLoggerImpl, type Log, type InfoLogEntry, type ErrorLogEntry} from "@wonder/core-core";
+import {LoggerImpl as CoreLoggerImpl, type Log, type InfoLogEntry, type ErrorLogEntry, type LoggerConfig as CoreLoggerConfig} from "@wonder/core-core";
 
 const OUTDATED_TIME_MILLISECONDS = 7 * 24 * 60 * 60 * 1000; // logs before 7 days is outdated
-export interface Logger {
-    addContext(context: {[key: string]: string | (() => string)}): void;
-    removeContext(key: string): void;
-    info(entry: InfoLogEntry): void;
-    warn(data: ErrorLogEntry): void;
-    error(data: ErrorLogEntry): void;
-}
+
+/**
+ * If eventLogger config is provided in non-DEV environment.
+ * All collected logs will automatically sent to {serverURL} in a regular basis.
+ *
+ * The request will be PUT to the server in the following format:
+ *      {events: Log[]}
+ *
+ * @param persist enable log persistence
+ * @param customPersistInstance use customized tool to persist logs, MMKV is the default solution
+ */
+
+type PersistConfig =
+    | {persist?: false; customPersistInstance?: never}
+    | {
+          persist: true;
+          customPersistInstance?: PersistInstance;
+      };
+
+export type LoggerConfig = PersistConfig & CoreLoggerConfig;
 
 export class LoggerImpl extends CoreLoggerImpl {
     private persistInstance: PersistInstance | undefined;
